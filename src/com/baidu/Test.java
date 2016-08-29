3fc4f8397d348878ab1642b9433f9dd7dde3a850
z137249025 <137249025@qq.com>
2016-08-29 11:53:08 +0800

    0.0.8

--- src/com/baidu/Test.java
+++ src/com/baidu/Test.java
@@ -7,12 +7,4 @@ public class Test {
 		System.out.println("aopioc");
 	}
 }
-package com.baidu;
 
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("abcdefg");
-		System.out.println("hello");
-	}
-}
